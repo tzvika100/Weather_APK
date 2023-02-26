31b98c8504f326bcce11a08e199531f071d4ef79
tzvika100 <tzvika100@gmail.com>
2023-02-26 20:11:06 +0200

    first_commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask , render_template , request
+#import urllib
+#import os
+import requests, json
+#import http.client
+
+app = Flask(__name__)
+
+@app.route('/' , methods=['GET' , 'POST'])
+def index():
+    if request.method=='POST':
+        city_name = request.form.get('city')
+        url = "https://yahoo-weather5.p.rapidapi.com/weather"
+
+        querystring = {"location":city_name,"format":"json","u":"C"}
+
+        headers = {
+            "X-RapidAPI-Key": "51b933430bmshd2a213d0a0aea60p1f91d4jsn3060816c96c9",
+            "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        my_data = response.json()
+        # return response.text
+        country =my_data['location']['country']
+        return render_template('index.html' , country = country)
+    else:
+        return render_template('index.html')
