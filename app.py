fbb01edafa02702846c4a9b6ad985b372a471b68
tzvika100 <tzvika100@gmail.com>
2023-03-25 19:25:35 +0300

    add port 5000

--- app.py
+++ app.py
@@ -26,3 +26,6 @@ def index():
         return render_template('index.html' , country = country)
     else:
         return render_template('index.html')
+
+if __name__== "__main__":
+    app.run(host='0.0.0.0' , port=5000 , debug = True)
